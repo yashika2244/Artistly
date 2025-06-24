763337c91fbd320c60153ba78fcc81935f77455e
Yashu Rajput <yashu@gmail.com>
2025-06-24 16:56:33 +0530

    4:56

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-[#0e1d70] text-white py-6 ">
+    <footer className="bg-[#310b77] text-white py-6 ">
       <div className="max-w-7xl mx-auto text-center text-sm">
         © 2025 Artistly.com | All rights reserved.
       </div>
